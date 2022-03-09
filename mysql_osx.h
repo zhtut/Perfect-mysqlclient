ece07e8a2c8a2fa8df02391505455c83dab8c6bd
周途广 <zhtg@icloud.com>
2022-03-09 21:01:48 +0800

    修复头文件引用错误的问题

--- mysql_osx.h
+++ mysql_osx.h
@@ -1,6 +1,6 @@
 #ifndef __PERFECT_MYSQL_OSX__
 #define __PERFECT_MYSQL_OSX__
-#include "/usr/local/include/mysql/mysql.h"
+#include "/usr/local/mysql/include/mysql.h"
 
 #if LIBMYSQL_VERSION_ID > 80000
 typedef char my_bool;
